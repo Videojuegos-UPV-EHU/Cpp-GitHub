2e84a703ff2339101ab6e1a2ff41117a940f178a
borjafdezgauna <borja.fernandez@ehu.eus>
2018-07-24 17:45:48 +0200

    Working on Class1

---
+++ GitHubTest/Class1.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include "Class1.h"
+#include <iostream>
+using namespace std;
+
+Class1::Class1()
+{
+}
+
+
+Class1::~Class1()
+{
+}
+
+void Class1::Print()
+{
+	int x = 0, y = -2;
+	cout << "x=" << x << "\n";
+	cout << "y=" << y << "\n";
+}
---
+++ GitHubTest/Class1.h
@@ -0,0 +1,11 @@
+#pragma once
+
+class Class1
+{
+public:
+	Class1();
+	virtual ~Class1();
+
+	void Print();
+};
+
